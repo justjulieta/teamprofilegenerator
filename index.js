8e9b6c6b59f4992b40e1d6f8063ed4d19c3b05c2
Julieta Ramirez Solis Nguyen <julieta.rammirez27@gmail.com>
2023-01-19 03:12:21 -0800

    Changes to be committed:

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
-const buildTeam = require('./src/buildTeam');
+const inquirer = require('inquirer');
+const path = require('path');
+const fs = require('fs');
+
+// Team Member Catagories
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+
+const DIST = path.resolve(__dirname, 'dist');
+const distPath = path.join(DIST, 'Myteam.html');
 
 // Function to initialize application:
 function init() {
